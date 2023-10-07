7af122af87451f22a9443662394689f3754fb7b0
D-NareshReddy <147236859+D-NareshReddy@users.noreply.github.com>
2023-10-07 14:30:05 +0530

    Update streamlit_app.py
    added pandas to read the tet file from S3 bucket.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text('🐔 Chicken')
 streamlit.text('🥑 Avocado')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
